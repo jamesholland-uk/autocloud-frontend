34c04e9f3d7cd2fac79a3d28a6745a35ab27862b
James Holland <jamoiholland@gmail.com>
2018-10-06 13:48:27 +0100

    push

--- status.php
+++ status.php
@@ -2,7 +2,37 @@
 <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta http-equiv="refresh" content="5" >
+    <?php 
+      // Database variables
+      $servername = "localhost";
+      $username = "dbuser";
+      $password = "Panadmin001!";
+      $dbname = "auto-hack-cloud";
+
+      // Create connection
+      $conn = new mysqli($servername, $username, $password, $dbname);
+      // Check connection
+      if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+      } 
+
+      // Collect job variable and get status
+      $uid=$_GET["uid"];
+      $sqlcheck = "SELECT * FROM jobs WHERE JOB = '$uid'";
+
+      $conn->query($sqlcheck);
+      $result = $conn->query($sqlcheck);
+      $status= $result->fetch_assoc();
+      
+      if($status['STATUS'] == "Done")
+      {
+        echo '';
+      }
+      else
+      {
+        echo '<meta http-equiv="refresh" content="5" >';
+      }
+    ?>
     <link rel="stylesheet" href="stylesheet.css" type="text/css">
     <title>Demo</title>
   </head>
@@ -13,25 +43,25 @@
 	<tr><td>&nbsp;</td><td>&nbsp;</td><td>
 <?php
 // Database variables
-$servername = "localhost";
-$username = "dbuser";
-$password = "Panadmin001!";
-$dbname = "auto-hack-cloud";
+//$servername = "localhost";
+//$username = "dbuser";
+//$password = "Panadmin001!";
+//$dbname = "auto-hack-cloud";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+//$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+//if ($conn->connect_error) {
+//    die("Connection failed: " . $conn->connect_error);
+//} 
 
 // Collect job variable and get status
-$uid=$_GET["uid"];
-$sqlcheck = "SELECT * FROM jobs WHERE JOB = '$uid'";
+//$uid=$_GET["uid"];
+//$sqlcheck = "SELECT * FROM jobs WHERE JOB = '$uid'";
 
-$conn->query($sqlcheck);
-$result = $conn->query($sqlcheck);
-$status= $result->fetch_assoc();
+//$conn->query($sqlcheck);
+//$result = $conn->query($sqlcheck);
+//$status= $result->fetch_assoc();
 
 // Display status
 echo "<br><br><br><h2><b>Status:&nbsp;&nbsp;&nbsp; </b><i>" . $status['STATUS'] . "</i></h2>\r\n";
@@ -98,9 +128,7 @@ Mischief:
   <li><a href="http://<?php echo $status['UNTRUSTIP' ]?>" target="_blank">PHP Web App and Web-Based Attacks</a>
   <li><a href="http://<?php echo $status['UNTRUSTIP' ]?>?uoGSo[]=%3Cscript%3Ealert(%E2%80%98BreakingPoint%E2%80%99)%3C/script%3E" target="_blank">Generic XSS</a>
   <li><a href="http://<?php echo $status['UNTRUSTIP' ]?>/graph.php?current_language=/../../../../../../../../etc/passwd.&module=Accounts&action=Import&parenttab=Support%5D" target="_blank">Generic Traversal and /etc/passwd Access</a> 
-
-
-
+  <li><a href="http://<?php echo $status['UNTRUSTIP'] ?>:4200" target="_blank">Console on Web Server</a>
 </ul>
 <br>
 <br>
