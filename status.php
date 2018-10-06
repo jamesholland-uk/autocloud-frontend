c217c5a21ab6c601e76de24d1c6609afb2e21065
James Holland <jamoiholland@gmail.com>
2018-10-06 13:04:17 +0100

    push

--- status.php
+++ status.php
@@ -79,23 +79,28 @@ if($status['STATUS'] == "Deploying" || $status['STATUS'] == "Bootstrapping" || $
 ?>
 Information:
 <ul>
-  <li><a href="process-summary.png" target="_blank">Deployment Process Summary Diagram</a><br>
-  <li><a href="network-diagram.png" target="_blank">Topology Diagram</a><br>
-  <li><a href="https://www.youtube.com/watch?v=DvLN-VH_xoo&feature=youtu.be" target="_blank">Manual Process Video</a><br>
+  <li><a href="process-summary.png" target="_blank">Deployment Process Summary Diagram</a>
+  <li><a href="network-diagram.png" target="_blank">Topology Diagram</a>
+  <li><a href="https://www.youtube.com/watch?v=DvLN-VH_xoo&feature=youtu.be" target="_blank">Manual Process Video</a>
 </ul>
 <?php
 } elseif($status['STATUS'] == "Done") {
 ?>
 Palo Alto Networks Virtual Appliances:
 <ul>
-	<li><a href="https://<?php echo $status['MGMTIP']  ?>" target="_blank">Web GUI for your brand new firewall!</a><br>
-	<li><a href="https://demomatic-rama-gcp.panw.co.uk" target="_blank">Web GUI for Panorama - Central Managemenet, Logging and Reporting</a><br>
+	<li><a href="https://<?php echo $status['MGMTIP']  ?>" target="_blank">Web GUI for your brand new firewall!</a>
+	<li><a href="https://demomatic-rama-gcp.panw.co.uk" target="_blank">Web GUI for Panorama - Central Managemenet, Logging and Reporting</a>
 </ul>
 Mischief:
 <ul>	
-  <li><a href="http://<?php echo $status['UNTRUSTIP'] ?>:8080" target="_blank">Java Web App</a><br>
-  <li><a href="http://<?php echo $status['KALIIP'] ?>:4200" target="_blank">Metasploit Console for Attacking Java Web App</a><br>
-  <li><a href="http://<?php echo $status['UNTRUSTIP' ]?>" target="_blank">PHP Web App and Web-Based Attacks</a><br>
+  <li><a href="http://<?php echo $status['UNTRUSTIP'] ?>:8080/struts2_2.3.15.1-showcase/showcase.action" target="_blank">Java Web App</a>
+  <li><a href="http://<?php echo $status['KALIIP'] ?>:4200" target="_blank">Metasploit Console for Attacking Java Web App</a>
+  <li><a href="http://<?php echo $status['UNTRUSTIP' ]?>" target="_blank">PHP Web App and Web-Based Attacks</a>
+  <li><a href="http://<?php echo $status['UNTRUSTIP' ]?>?uoGSo[]=%3Cscript%3Ealert(%E2%80%98BreakingPoint%E2%80%99)%3C/script%3E" target="_blank">Generic XSS</a>
+  <li><a href="http://<?php echo $status['UNTRUSTIP' ]?>/graph.php?current_language=/../../../../../../../../etc/passwd.&module=Accounts&action=Import&parenttab=Support%5D" target="_blank">Generic Traversal and /etc/passwd Access</a> 
+
+
+
 </ul>
 <br>
 <br>
@@ -125,7 +130,7 @@ echo "Email:" . $status['EMAIL'] . "<br>";
 echo "Nickname: " . $status['NICKNAME'] . "<br></font><br><br>";
 
 if($status['STATUS'] == "Done") {
-echo '<b><a href=index.html><img src=startagain.png alt="Start Again"></a><b><br><br><br><br>';
+echo '<b><a href=index.html><img src=startagain.jpg alt="Start Again"></a><b><br><br><br><br>';
 }
 
 $conn->close();
